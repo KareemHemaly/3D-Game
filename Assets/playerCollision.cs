7eb0b4947af2c2bc9ed272c6d61a50e1ca24b290
sa3d clay <sa3d.clay@gmail.com>
2019-04-30 04:03:15 +0200

    walk back : (

--- Assets/playerCollision.cs
+++ Assets/playerCollision.cs
@@ -8,9 +8,24 @@ public class playerCollision : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-
         Debug.Log(collision.collider.name);
+
+        
+        // force is how forcefully we will push the player away from the enemy.
+        float force = 3;
+
+        // If the object we hit is the building
+        if (collision.gameObject.tag == "build")
+        {
+            // Calculate Angle Between the collision point and the player
+            Vector3 dir = collision.contacts[0].point - transform.position;
+            // We then get the opposite (-Vector3) and normalize it
+            dir = -dir.normalized;
+            // And finally we add force in the direction of dir and multiply it by force. 
+            // This will push back the player
+            GetComponent<Rigidbody>().AddForce(dir * force);
+
+        }
+        
     }
-
-
 }
